1c8b0cfe905434025192da0541e5d4ee97312f28
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-05-31 03:40:42 +0200

    update

--- i3/1-install-i3.sh
+++ i3/1-install-i3.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo apt install i3 i3lock i3status dmenu feh -y
+sudo apt install i3 i3lock i3status dmenu feh lxappearance compton j4-dmenu-desktop gsimplecal -y
+
+cd && sudo rm -rf bumblebee-status
+git clone git://github.com/tobi-wan-kenobi/bumblebee-status
---
+++ theming/fonts/README.md
@@ -0,0 +1,5 @@
+# Fonts
+
+## Installation
+
+Create a new folder in the `home/<user>` folder with name: `.fonts`, and copy the fonts there.
