8daba3a77807580a37e2fc3517d116e94196ca0d
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-06-01 15:38:42 +0200

    github push script

---
+++ github-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote set-url origin https://github.com/acestojanoski/linux-setup-scripts.git
+git push origin master
+git remote set-url origin https://gitlab.com/acestojanoski/linux-setup-scripts.git
