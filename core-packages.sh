bcfa48c535566e4c15987e55371d89c20090886f
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-06-01 13:52:35 +0200

    update

--- core-packages.sh
+++ core-packages.sh
@@ -4,7 +4,7 @@ echo "Updating package lists"
 sudo apt update
 
 echo "Installing core packages"
-sudo apt install vlc terminator build-essential vim rofi shutter meld chromium-browser -y
+sudo apt install vlc terminator build-essential vim rofi shutter meld chromium-browser neofetch -y
 
 echo "Installing file compressors"
 sudo apt install zip unzip rar unrar -y
