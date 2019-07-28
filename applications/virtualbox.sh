0ac06822a159b974e39c3d581e4e28b2a9e74e48
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-07-28 17:35:19 +0200

    virtualbox update

--- applications/virtualbox.sh
+++ applications/virtualbox.sh
@@ -4,7 +4,7 @@ if [[ -f /tmp/virtualbox_amd64.deb ]]; then
 	sudo rm /tmp/virtualbox_amd64.deb
 fi
 
-sudo wget https://download.virtualbox.org/virtualbox/6.0.6/virtualbox-6.0_6.0.6-130049~Ubuntu~bionic_amd64.deb -O /tmp/virtualbox_amd64.deb
+sudo wget https://download.virtualbox.org/virtualbox/6.0.10/virtualbox-6.0_6.0.10-132072~Ubuntu~bionic_amd64.deb -O /tmp/virtualbox_amd64.deb
 
 sudo dpkg -i /tmp/virtualbox_amd64.deb
 sudo apt install -f
