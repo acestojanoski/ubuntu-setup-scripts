ffe39c836892642ed10864b49c08704592b870c7
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-09-05 00:11:30 +0200

    update

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-# install vim
-sudo apt install vim
+echo
+echo "####################"
+echo "update package lists"
+echo "####################"
 
-# install pathogen
+sudo apt update
+
+echo
+echo "###########"
+echo "install vim"
+echo "###########"
+
+sudo apt install vim -y
+
+echo
+echo "################"
+echo "install pathogen"
+echo "################"
+
 if [[ -d ~/.vim ]]; then
     sudo rm -rf ~/.vim
 fi
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo
+echo "################"
+echo "install plugins"
+echo "################"
 
 # install nerdtree
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
@@ -26,21 +46,30 @@ git clone https://github.com/dense-analysis/ale.git ~/.vim/bundle/ale
 
 # install YouCompleteMe
 git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-sudo apt install build-essential cmake python3-dev
+sudo apt install build-essential cmake python3-dev -y
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 sudo python3 install.py --ts-completer
 
-# get config from my repository
+echo
+echo "#############################"
+echo "get config from my repository"
+echo "#############################"
+
 if [[ -f /tmp/.vimrc ]]; then
     sudo rm /tmp/.vimrc
 fi
 
 sudo wget https://raw.githubusercontent.com/acestojanoski/config-files/master/linux/home/.vimrc -O /tmp/.vimrc
 
-# remove old config if exists and replace it with the new one
+echo
+echo "###############################################################"
+echo "remove the old config if exists and replace it with the new one"
+echo "###############################################################"
+
 if [[ -f ~/.vimrc ]]; then
     sudo rm ~/.vimrc
 fi
 
 sudo cp /tmp/.vimrc ~/
-sudo rm /tmp/.vimcr
+sudo rm /tmp/.vimrc
+
