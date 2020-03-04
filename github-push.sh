46ae31b019eb6ca4e2542dd0122e26c365fdd16b
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2020-03-04 22:20:18 +0100

    cleanup

--- github-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git remote set-url origin https://github.com/acestojanoski/linux-setup-scripts.git
-git push origin master
-git remote set-url origin https://gitlab.com/acestojanoski/linux-setup-scripts.git
